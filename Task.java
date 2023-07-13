538e7462ee3ab1046e04dfb584ea27c549a29e26
gnarus-tm <71601039+gna-rus@users.noreply.github.com>
2023-07-13 09:55:44 +0300

    Update Task.java

--- Task.java
+++ Task.java
@@ -7,21 +7,31 @@ public class Task
      public static HashSet GenerateHashSet(int count)
     {
     HashSet < Object > ObjSet = new HashSet <> ();
-    System.out.print("Введите id: ");
     Scanner scanner = new Scanner(System.in);
-    int num = scanner.nextInt();
+    
+    System.out.println("Введите вес выпадения: ");
+    int numWeight = scanner.nextInt();
+    
+    System.out.println("Введите название игрушки: ");
     String nameToy = scanner.nextLine();
-    ClassOfStore NoteBook1 = new ClassOfStore (count++, num, nameToy);
+    
+    ClassOfStore NoteBook1 = new ClassOfStore (count++, numWeight, nameToy);
     ObjSet.add (NoteBook1);
     return ObjSet;
     }
   
+    public static void print_menu() {
+        // Меню
+        System.out.println("Меню фильтрации: \n1 - По названию ноутбука;\n2 - По ОС ноутбука;");
+        System.out.println("3 - По цвету ноутбука; \n4 - Минимальным параметрам; \n5 - Очистить фильтр поиск;");
+        System.out.println("6 - Выход.");
+    }  
  
  
     public static void main (String[]args)
     {
     HashSet < Object > ObjSet1 = new HashSet <> ();
-    //ObjSet1 = GenerateHashSet ();
+    print_menu();
     int count = 0;
     int num = 0;
         while (true) {
