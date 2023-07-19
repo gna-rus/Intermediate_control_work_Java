244c79722514e85a9e41894415395d7013e4130c
Nickolay Grigoriev <gnarus@inbox.ru>
2023-07-19 20:53:49 +0300

    Функция изменения количества игрушек на скоале добавлена

--- ClassOfStore.java
+++ ClassOfStore.java
@@ -1,60 +1,50 @@
-import java.util.HashMap;
 import java.util.LinkedList;
 
-class ClassOfStore {
+public class ClassOfStore {
     private int count;
     private int num;
     private String nameToy; // название игрушки
     private int rndQuantityInStock;
-   
-   public ClassOfStore (int count, int num, String  nameToy, int rndQuantityInStock)
-  {
-    this.count = count;
-    this.num = num;
-    this.nameToy = nameToy;
-    this.rndQuantityInStock = rndQuantityInStock;
-  }
-  
-  public int getCount ()
-  {
-    return count;
-  }
 
-  public void setCount (int count)
-  {
-    this.count = count;
-  }
-  
-  
-  public int getNum ()
-  {
-    return num;
-  }
+    public ClassOfStore(int count, int num, String nameToy, int rndQuantityInStock) {
+        this.count = count;
+        this.num = num;
+        this.nameToy = nameToy;
+        this.rndQuantityInStock = rndQuantityInStock;
+    }
 
-  public void setNum (int num)
-  {
-    this.num = num;
-  }
-  
-  public String GetNameToy ()
-  {
-    return nameToy;
-  }
+    public int getCount() {
+        return count;
+    }
 
-  public void SetNameToy (String nameToy)
-  {
-    this.nameToy = nameToy;
-  }
-  
-    public int getQuantity ()
-  {
-    return rndQuantityInStock;
-  }
+    public void setCount(int count) {
+        this.count = count;
+    }
 
-  public void setQuantity (int rndQuantityInStock)
-  {
-    this.rndQuantityInStock = rndQuantityInStock;
-  }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String GetNameToy() {
+        return nameToy;
+    }
+
+    public void SetNameToy(String nameToy) {
+        this.nameToy = nameToy;
+    }
+
+    public int getQuantity() {
+        return rndQuantityInStock;
+    }
+
+    public void setQuantity(int rndQuantityInStock) {
+        this.rndQuantityInStock = rndQuantityInStock;
+    }
 
     @Override
     public String toString() {
@@ -62,13 +52,13 @@ class ClassOfStore {
                 + "id = " + count
                 + ", weight = " + num
                 + ", Name = " + nameToy
-                + ", quantity_in_stock = " + rndQuantityInStock  + '.';
+                + ", quantity_in_stock = " + rndQuantityInStock + '.';
     }
-    
-    
+
+
     public LinkedList toLink() {
-    LinkedList<String> toLinkToyStore = new LinkedList<>();
-    toLinkToyStore.add(nameToy);
-    return toLinkToyStore;
+        LinkedList<String> toLinkToyStore = new LinkedList<>();
+        toLinkToyStore.add(nameToy);
+        return toLinkToyStore;
     }
 }
--- Task.java
+++ Task.java
@@ -1,74 +1,73 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.LinkedList;
-import java.util.ArrayList;
-import java.util.Random;
 import java.util.PriorityQueue;
-import java.io.*;
+import java.util.Random;
+import java.util.Scanner;
 
-public class Task
-{
+public class Task {
     // Фунция заполнения Класса
-     public static ClassOfStore GenerateHashSet(int numId)
-    {
-    System.out.println("Введите вес выпадения: ");
-    Scanner scanner = new Scanner(System.in);
-    int numWeight = scanner.nextInt();
-    
-    System.out.println("Введите название игрушки: ");
-    Scanner scanner1 = new Scanner(System.in);
-    String nameToy = scanner1.nextLine();
-    
-    Random random = new Random();
-    int rndQuantityInStock = random.nextInt(12)+5;
-    System.out.printf("Количество %s на складе: %d \n", nameToy, rndQuantityInStock);
-    
-    ClassOfStore toyFromStore = new ClassOfStore (numId, numWeight, nameToy, rndQuantityInStock);
-    //System.out.println(toyFromStore.getNum());
-    return toyFromStore;
+    public static ClassOfStore GenerateHashSet(int numId) {
+        System.out.println("Введите вес выпадения: ");
+        Scanner scanner = new Scanner(System.in);
+        int numWeight = scanner.nextInt();
+
+        System.out.println("Введите название игрушки: ");
+        Scanner scanner1 = new Scanner(System.in);
+        String nameToy = scanner1.nextLine();
+
+        Random random = new Random();
+        int rndQuantityInStock = random.nextInt(12) + 5;
+        System.out.printf("Количество %s на складе: %d \n", nameToy, rndQuantityInStock);
+
+        ClassOfStore toyFromStore = new ClassOfStore(numId, numWeight, nameToy, rndQuantityInStock);
+        toyFromStore.setCount(numId);
+        toyFromStore.setNum(numWeight);
+        toyFromStore.SetNameToy(nameToy);
+        toyFromStore.setQuantity(rndQuantityInStock);
+        //System.out.println(toyFromStore.getNum());
+        return toyFromStore;
     }
-    
+
     // Меню
     public static void print_menu() {
-        
+
         System.out.println("Меню: \n1 - Новая игрушка;\n2 - Перечень всех игрушек;");
         System.out.println("3 - Провести розыгрыш; \n4 - Запись в файл результатов;");
         System.out.println("5 - Изменить вес игрушки; \n6 - Выход.");
     }
-    
+
     public static void print_all_toys(LinkedList ObjSet1) {
-        
+
         for (Object str1 : ObjSet1) {
             System.out.println(str1.toString());
         }
     }
-    
+
     // функция генерации Link
-    public static LinkedList generatorOfLink(String Toy)
-    {
+    public static LinkedList generatorOfLink(String Toy) {
         LinkedList<String> toLinkToyStore = new LinkedList<>();
         toLinkToyStore.add(generatorOfElem(Toy, "id"));
         toLinkToyStore.add(generatorOfElem(Toy, "weight"));
         toLinkToyStore.add(generatorOfElem(Toy, "Name"));
+        toLinkToyStore.add(generatorOfElem(Toy, "quantity_in_stock"));
         return toLinkToyStore;
     }
-    
+
     // функция поиска элемента в строке через определение индексов
-    public static String generatorOfElem(String Toy, String Elem)
-    {
+    public static String generatorOfElem(String Toy, String Elem) {
         int firstIndex = FindFirstIndexOf(Toy, Elem);
-        int finalIndex = firstIndex + Elem.length()+2;
+        int finalIndex = firstIndex + Elem.length() + 2;
         String elem = FindLastIndexOf(Toy, finalIndex);
         return elem.trim();
     }
-    
+
     // поиск первого индекса подстроки в строке
     public static int FindFirstIndexOf(String BigStr, String SmallStr) {
         int index = BigStr.lastIndexOf(SmallStr);
         return index;
     }
-    
+
     // функция поиска значения параметра через индексы строки
     public static String FindLastIndexOf(String BigStr, int StartIndex) {
         String word = "";
@@ -83,11 +82,10 @@ public class Task
         }
         return word;
     }
-    
+
     //////////////////////
     // функция розыгрыша приза (будет осуществляться через массив)
-    public static String make_calkulation(LinkedList ObjSet1)
-    {
+    public static String make_calkulation(LinkedList ObjSet1) {
         LinkedList<String> toLinkToy = new LinkedList<>();
         int lengthWeight = 0;
         for (Object str1 : ObjSet1) {
@@ -96,92 +94,107 @@ public class Task
         }
         String[] arrRoulette = new String[lengthWeight]; // массив-рулекта
         arrRoulette = arrFill(ObjSet1, arrRoulette); // заполняю массиву-рулетку названиями игрушек
-        String elem = arrRandomElem(arrRoulette);
+        String elem = arrRandomElem(arrRoulette); // через рандом определяю победителя
         System.out.println("Игрушка победитель: " + elem);
         return elem;
-        
+
     }
-    
+
     //функция заполнения массива названиями игрушек
-    public static String[] arrFill(LinkedList ObjSet1, String[]arrRoulette)
-    {
+    public static String[] arrFill(LinkedList ObjSet1, String[] arrRoulette) {
         int count = 0;
         LinkedList<String> toLinkToy = new LinkedList<>();
-        for (Object str1 : ObjSet1){
+        for (Object str1 : ObjSet1) {
             toLinkToy = generatorOfLink(str1.toString());
-            for (int i = 0; i < Integer.parseInt(toLinkToy.get(1)); i++){
+            for (int i = 0; i < Integer.parseInt(toLinkToy.get(1)); i++) {
                 arrRoulette[count] = toLinkToy.get(2);
                 count++;
             }
         }
         return arrRoulette;
     }
-    
+
     // функция извлечение случайного элемента из массива-рулетки
-    public static String arrRandomElem(String[]arrRoulette)
-    {
+    public static String arrRandomElem(String[] arrRoulette) {
         Random random = new Random();
         int randomIndex = random.nextInt(arrRoulette.length);
         return arrRoulette[randomIndex];
     }
-    
+
     // Функция записи результата в файл
-    public static void work_with_PriorityQueue(PriorityQueue ObjSet1)
-    {
-        for (Object elem: ObjSet1){
+    public static void work_with_PriorityQueue(PriorityQueue ObjSet1) {
+        for (Object elem : ObjSet1) {
             System.out.println(elem);
         }
-        
-        try(FileWriter writer = new FileWriter("result.txt", false))
-        {
-            for (Object elem: ObjSet1){
-            // запись всей строки
-            writer.write(elem.toString()+"\n");
-            
-            writer.flush(); // очистить буфер без прерывания сессии программы
-        }
+
+        try (FileWriter writer = new FileWriter("result.txt", false)) {
+            for (Object elem : ObjSet1) {
+                // запись всей строки
+                writer.write(elem.toString() + "\n");
+
+                writer.flush(); // очистить буфер без прерывания сессии программы
+            }
         }
         // если не получилось записать то выдает ошибку в консоль без прерывания сессии
-        catch(IOException ex){
-             
+        catch (IOException ex) {
+
             System.out.println("Error!!!\n" + ex.getMessage());
-        } 
-    }
-    
-    // Функция изменения веса игрушки
-    public static void changeWeightToy(LinkedList ObjSet1)
-    {
-        System.out.println("Изменить вес игрушки");
-        for (Object elem: ObjSet1)
-        {
-            System.out.println(elem.getNum());
         }
     }
- 
- 
-    public static void main (String[]args)
-    {
 
-    LinkedList <ClassOfStore> ObjSet1 = new LinkedList <> ();
-    PriorityQueue< String> PriorityQueueResult = new PriorityQueue<>();
-    Scanner scanner = new Scanner(System.in);
-    print_menu();
-    int count = 1;
-     int num = 0;
+    // Функция изменения веса игрушки
+    public static void changeWeightToy(LinkedList ObjSet1) {
+        LinkedList<String> LinkToyStore = new LinkedList<>();
+        System.out.println("Изменить вес игрушки");
+        for (Object elem : ObjSet1) {
+            LinkToyStore = generatorOfLink(elem.toString());
+            System.out.println(LinkToyStore.get(2));
+        }
+    }
+
+    // Функция изменения количества игрушек на скоале
+    public static void changeQuantityToy(LinkedList ObjSet1, String Toy) {
+        LinkedList<String> LinkToyStore = new LinkedList<>(); // генерирую коллекцию
+        LinkedList<String> LinkToyStore2 = new LinkedList<>();
+
+        for (Object elem : ObjSet1) {
+            LinkToyStore = generatorOfLink(elem.toString());// делаю LinkedList каждого обьекта класса
+            for (String elem1 : LinkToyStore) { // в каждома обьекте класса ищу необходимое название игрушки
+                if (elem1.equals(Toy)) {
+                    LinkToyStore2.add(Integer.toString(Integer.parseInt(elem1) - 1)); // изменяю количество
+                }
+                else {LinkToyStore2.add(elem1);}
+            }
+        }
+        // создаю новый образец класса с новыми параметрами
+        ClassOfStore toyFromStore = new ClassOfStore(Integer.parseInt(LinkToyStore2.get(0)), Integer.parseInt(LinkToyStore2.get(1)), LinkToyStore2.get(2), Integer.parseInt(LinkToyStore2.get(3)));
+    }
+
+
+    public static void main(String[] args) {
+
+        LinkedList<ClassOfStore> ObjSet1 = new LinkedList<>();
+        PriorityQueue<String> PriorityQueueResult = new PriorityQueue<>();
+        Scanner scanner = new Scanner(System.in);
+        print_menu();
+        int count = 1;
+        int num = 0;
         while (true) {
-            
+
             System.out.print("Введите число: ");
             num = scanner.nextInt();
-            if (num == 6){break;}
+            if (num == 6) {
+                break;
+            }
             switch (num) {
                 case 1:
-                    ObjSet1.add(GenerateHashSet(count++));
+                    ObjSet1.addFirst(GenerateHashSet(count++));
                     continue;
                 case 2:
                     print_all_toys(ObjSet1);
                     continue;
                 case 3:
-                    PriorityQueueResult.add(make_calkulation(ObjSet1));// провожу лотырею и записываю результат ф Очередь
+                    PriorityQueueResult.add(make_calkulation(ObjSet1));// провожу лотырею и записываю результат в Очередь
                     continue;
                 case 4:
                     work_with_PriorityQueue(PriorityQueueResult);// передаю очередь для записи в файл
@@ -189,9 +202,9 @@ public class Task
                 case 5:
                     changeWeightToy(ObjSet1);// передаю очередь для записи в файл
                     break;
-                
+
             }
-    
-    } 
-}
+
+        }
+    }
 }
